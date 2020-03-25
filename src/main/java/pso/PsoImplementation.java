d2f41dc6f5415901210750f4067e61a8e7808249
Mounika Madela <mounikamadela@gmail.com>
2020-03-25 22:16:07 +0000

    Added more code for cloud simulation:

--- src/main/java/pso/PsoImplementation.java
+++ src/main/java/pso/PsoImplementation.java
@@ -10,53 +10,65 @@ import org.cloudbus.cloudsim.hosts.Host;
 import org.cloudbus.cloudsim.hosts.HostSimple;
 import org.cloudbus.cloudsim.resources.Pe;
 import org.cloudbus.cloudsim.resources.PeSimple;
+import org.cloudbus.cloudsim.vms.Vm;
+import org.cloudbus.cloudsim.vms.VmSimple;
 
 public class PsoImplementation {
 
+	static CloudSim cloudSim;
+	static Vm[][] vmMatrix;
+	
+	
 	public static void main(String[] args) {
+		
+		initializeCloud();
+		cloudSim.start();
 		
 		Swarm swarm = new Swarm();
 
 		swarm.runSwarm();	
+		
+		cloudSim.terminate();
 
 	}
 	
-	public void initializeCloud() {
-		Datacenter dc = createDatacenter("DataCenter_0");
-
+	public static void initializeCloud() {
+		Datacenter dc = createDatacenter("DataCenter_0",10,50);
+		
 	}
 
-	private static Datacenter createDatacenter(String name){
+	private static Datacenter createDatacenter(String name, int sizeOfVms, int sizeOfHosts){
 
-		CloudSim cloudSim  = new CloudSim();
-		// Here are the steps needed to create a PowerDatacenter:
-		// 1. We need to create a list to store
-		//    our machine
+		vmMatrix = new Vm[sizeOfHosts][sizeOfVms];
+		cloudSim  = new CloudSim();
 		List<Host> hostList = new ArrayList<Host>();
 
-		// 2. A Machine contains one or more PEs or CPUs/Cores.
-		// In this example, it will have only one core.
-		List<Pe> peList = new ArrayList<Pe>();
+		for(int i = 0; i<sizeOfHosts; i++) {
+			for(int j=0; j<sizeOfVms; j++) {
+				
+				List<Pe> peList = new ArrayList<Pe>();
 
-		int mips = 1000;
+				int mips = 1000;
+				// 3. Create PEs and add these into a list.
+				peList.add(new PeSimple(020000)); // need to store Pe id and MIPS Rating
+
+				//4. Create Hosts with its id and list of PEs and add them to the list of machines
+				int ram = 2048; //host memory (MB)
+				long storage = 1000000; //host storage
+				int bw = 10000;
+				Host host = new HostSimple(
+						ram, bw, storage, peList
+					);
+				hostList.add(host);
+				
+				Vm vm = new VmSimple(1000,1);
+				vm.setRam(1000).setBw(1000).setSize(1000);
+				host.addMigratingInVm(vm);
+				vmMatrix[i][j]=vm;				
+			}
+		}
 		
-
-		// 3. Create PEs and add these into a list.
-		peList.add(new PeSimple(020000)); // need to store Pe id and MIPS Rating
-
-		//4. Create Hosts with its id and list of PEs and add them to the list of machines
-		int hostId=0;
-		int ram = 2048; //host memory (MB)
-		long storage = 1000000; //host storage
-		int bw = 10000;
-
-		hostList.add(
-    			new HostSimple(
-    				ram, bw, storage, peList
-    			)
-    		); // This is our first machine
-
-		
+	
 
 		Datacenter datacenter = new DatacenterSimple(cloudSim, hostList);
 
